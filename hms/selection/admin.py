7cc9a9acb125458bd2cc84bce35be259b3c9fd0b
Aayush Chaudhry <33194760+maverick0004@users.noreply.github.com>
2018-05-22 21:08:21 +0530

    Update admin.py

--- hms/selection/admin.py
+++ hms/selection/admin.py
@@ -3,12 +3,12 @@ from .models import Student,Room,Hostel
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-	list_display=['student_name','father_name','gender','enrollment_no','dob','current_sem','room','room_alloted']
+	list_display = ['student_name','father_name','gender','enrollment_no','dob','current_sem','room','room_alloted']
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-	list_display=['no','name','room_type','vacant','hostel']
+	list_display = ['no','name','room_type','vacant','hostel']
 
 @admin.register(Hostel)
 class HostelAdmin(admin.ModelAdmin):
-	list_display=['name','warden','caretaker']
+	list_display = ['name','warden','caretaker']
