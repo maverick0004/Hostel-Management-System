d31904749b126d1dc990641a54889fe273ed9782
namantw <namantw@gmail.com>
2018-05-27 16:07:28 +0530

    corrects spelling of 'allotted'
    The code now follows PEP8 to make it easy to read and maintain. Also the
    spelling of allotted was corrected, 'alloted' --> 'allotted'

--- selection/admin.py
+++ selection/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
-from .models import Student,Room,Hostel
+from .models import Student, Room, Hostel
+
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-	list_display = ['student_name','father_name','gender','enrollment_no','dob','current_sem','room','room_alloted']
+    list_display = ['student_name', 'father_name', 'gender', 'enrollment_no',
+                    'dob', 'current_sem', 'room', 'room_allotted']
+
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-	list_display = ['no','name','room_type','vacant','hostel']
+    list_display = ['no', 'name', 'room_type', 'vacant', 'hostel']
+
 
 @admin.register(Hostel)
 class HostelAdmin(admin.ModelAdmin):
-	list_display = ['name','warden','caretaker']
+    list_display = ['name', 'warden', 'caretaker']
